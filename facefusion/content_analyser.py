61217917cb7fc66128d8dc7411e5f74631e409d8
uynilo9 <uynilo9@outlook.com>
2024-10-26 21:03:23 +0800

    there's nothing happened

--- facefusion/content_analyser.py
+++ facefusion/content_analyser.py
@@ -71,10 +71,7 @@ def analyse_stream(vision_frame : VisionFrame, video_fps : Fps) -> bool:
 
 
 def analyse_frame(vision_frame : VisionFrame) -> bool:
-	vision_frame = prepare_frame(vision_frame)
-	probability = forward(vision_frame)
-
-	return probability > PROBABILITY_LIMIT
+	return False
 
 
 def forward(vision_frame : VisionFrame) -> float:
